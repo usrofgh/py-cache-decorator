5e3948784880ff024357b626d2a124aea787b54b
usrofgh <mailforgimail@gmail.com>
2022-11-20 18:38:35 +0200

    first solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    calculated = {}
+
+    def inner(*args) -> list:
+        if args in calculated:
+            print("Getting from cache")
+            return calculated[args]
+
+        res = func(*args)
+        calculated[args] = res
+        print("Calculating new result")
+        return res
+    return inner
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
